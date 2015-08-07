918c89506a7984c4a3ae3896ab146ca4e3f6da9b
tigershen23 <tigershen23@gmail.com>
2015-08-07 11:53:47 -0700

    Add ajax form submit

--- js/script.js
+++ js/script.js
@@ -221,44 +221,52 @@ function parallax() {
 /*	contact form
 ------------------------------------------------------*/
 
-//    $('form#contactForm button.submit').click(function() {
-//
-//       $('#image-loader').fadeIn();
-//
-//       var contactName = $('#contactForm #contactName').val();
-//       var contactEmail = $('#contactForm #contactEmail').val();
-//       var contactSubject = $('#contactForm #contactSubject').val();
-//       var contactMessage = $('#contactForm #contactMessage').val();
-//
-//       var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-//                '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-//
-//       $.ajax({
-//
-// 	      type: "POST",
-// 	      url: "inc/sendEmail.php",
-// 	      data: data,
-// 	      success: function(msg) {
-//
-//             // Message was sent
-//             if (msg == 'OK') {
-//                $('#image-loader').fadeOut();
-//                $('#message-warning').hide();
-//                $('#contactForm').fadeOut();
-//                $('#message-success').fadeIn();
-//             }
-//             // There was an error
-//             else {
-//                $('#image-loader').fadeOut();
-//                $('#message-warning').html(msg);
-// 	            $('#message-warning').fadeIn();
-//             }
-//
-// 	      }
-//
-//       });
-//       return false;
-//    });
-//
-//
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      // var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      // var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+              //  '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "//formspree.io/tigershen23@gmail.com",
+	      data: {
+          name: contactName,
+          email: contactEmail,
+          message: contactMessage,
+          next: 'google.com'
+        },
+        dataType: "json",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+	      success: function(msg) {
+            // Message was sent
+            if (msg.success == 'email sent') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+      return false;
+   });
+
 });
